6802e00f17c53e959edc3f96f3cc9a7ac3f7432f
tkdtn4657 <tkdtn101226@gmail.com>
2023-05-09 15:13:40 +0900

    init

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -47,7 +47,7 @@ public class NotProd {
                 Member memberUser4 = memberService.join("user4", "1234").getData();
                 Member memberUser5 = memberService.join("user5", "1234").getData();
 
-                Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__%s".formatted(kakaoDevUserOAuthId)).getData();
+                Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2745252925".formatted(kakaoDevUserOAuthId)).getData();
                 Member memberUser7ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__%s".formatted(googleDevUserOAuthId)).getData();
                 Member memberUser8ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__%s".formatted(naverDevUserOAuthId)).getData();
                 Member memberUser9ByFacebook = memberService.whenSocialLogin("FACEBOOK", "FACEBOOK__%s".formatted(facebookDevUserOAuthId)).getData();
