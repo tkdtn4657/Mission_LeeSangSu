f296a8d5792084eab89e918215809d5f8ae2f4e4
tkdtn4657 <tkdtn101226@gmail.com>
2023-05-10 13:50:12 +0900

    배포를 위해 이전에 테스트를 위해 변경했던 NotProd내용 원래 상태로 변경 -> TestCase AllPass

--- records/4Week_LeeSangSu.md
+++ records/4Week_LeeSangSu.md
@@ -23,10 +23,14 @@ https://localhost/usr/likeablePerson/toList?gender=M&attractiveTypeCode=&sortCod
 likeablePerson/toList - likeableController 부터 체크
 if(gender)의 조건에 W와 M에 따른 각 성별에 따른 리스트를 갖도록 로직변경
 stream().filter.equals()를 활용해서 특정 성별을 필터링 -> 리스트 출력
-
+2. 네이버 클라우드 배포
+네이버 클라우드에 CentOS7.8버전으로 생성 후 java, 도커, sql등등 설치 후에 배포!
 **[특이사항]**
 
 어떻게 필터를 걸어야 할지 고민을 했는데 멘토님께 힌트를 받아서 일부 해결
 로직을 제대로 이해하지 못해서 발생한 문제였다.
 멘토님의 힌트에 도움받아 stream().filter().equals()를 통해서 하나의 성별을 필터해서 걸 수 있었다.
 조건문의 else를 최대한 활용하지 않으며 직관성 향상에 신경썼다
+
+네이버 클라우드 배포하는 부분은 필수미션1을 해결하기 위해 NotProd 부분을 수정해서 체크했었는데 
+이 때문에 리눅스에서 Test Build할 때 문제가 생기게 되어 실패하는 상황이 있었다.
--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -45,12 +45,7 @@ public class NotProd {
                 Member memberUser2 = memberService.join("user2", "1234").getData();
                 Member memberUser3 = memberService.join("user3", "1234").getData();
                 Member memberUser4 = memberService.join("user4", "1234").getData();
-                Member memberUser6 = memberService.join("user6", "1234").getData();
-                Member memberUser7 = memberService.join("user7", "1234").getData();
-                Member memberUser8 = memberService.join("user8", "1234").getData();
-                Member memberUser9 = memberService.join("user9", "1234").getData();
-                Member memberUser10 = memberService.join("user10", "1234").getData();
-
+                Member memberUser5 = memberService.join("user5", "1234").getData();
 
                 Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2745252925".formatted(kakaoDevUserOAuthId)).getData();
                 Member memberUser7ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__%s".formatted(googleDevUserOAuthId)).getData();
@@ -59,27 +54,104 @@ public class NotProd {
 
                 instaMemberService.connect(memberUser2, "insta_user2", "M");
                 instaMemberService.connect(memberUser3, "insta_user3", "W");
-                instaMemberService.connect(memberUser4, "e_hana_na", "M");
-                instaMemberService.connect(memberUser6, "insta_user6", "W");
-                instaMemberService.connect(memberUser7, "insta_user7", "W");
-                instaMemberService.connect(memberUser8, "insta_user8", "W");
-                instaMemberService.connect(memberUser9, "insta_user9", "M");
-                instaMemberService.connect(memberUser10, "insta_user10", "W");
+                instaMemberService.connect(memberUser4, "insta_user4", "M");
+                instaMemberService.connect(memberUser5, "insta_user5", "W");
 
                 // 원활한 테스트와 개발을 위해서 자동으로 만들어지는 호감이 삭제, 수정이 가능하도록 쿨타임해제
-                LikeablePerson likeablePersonToInstaUser4 = likeablePersonService.like(memberUser3, "e_hana_na", 1).getData();
+                LikeablePerson likeablePersonToInstaUser4 = likeablePersonService.like(memberUser3, "insta_user4", 1).getData();
                 Ut.reflection.setFieldValue(likeablePersonToInstaUser4, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
                 LikeablePerson likeablePersonToInstaUser100 = likeablePersonService.like(memberUser3, "insta_user100", 2).getData();
                 Ut.reflection.setFieldValue(likeablePersonToInstaUser100, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
 
                 LikeablePerson likeablePersonToInstaUserAbcd = likeablePersonService.like(memberUser3, "insta_user_abcd", 2).getData();
-                LikeablePerson likeablePersonToInstaUser6 = likeablePersonService.like(memberUser6, "e_hana_na", 1).getData();
-                LikeablePerson likeablePersonToInstaUser7 = likeablePersonService.like(memberUser7, "e_hana_na", 3).getData();
-                LikeablePerson likeablePersonToInstaUser8 = likeablePersonService.like(memberUser8, "e_hana_na", 1).getData();
-                LikeablePerson likeablePersonToInstaUser9 = likeablePersonService.like(memberUser9, "e_hana_na", 3).getData();
-                LikeablePerson likeablePersonToInstaUser10 = likeablePersonService.like(memberUser10, "e_hana_na", 2).getData();
-
             }
         };
     }
 }
+
+
+//package com.ll.gramgram.base.initData;
+//
+//import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+//import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+//import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+//import com.ll.gramgram.boundedContext.member.entity.Member;
+//import com.ll.gramgram.boundedContext.member.service.MemberService;
+//import com.ll.gramgram.standard.util.Ut;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.LocalDateTime;
+//
+//@Configuration
+//@Profile({"dev", "test"})
+//public class NotProd {
+//    @Value("${custom.security.oauth2.client.registration.kakao.devUserOauthId}")
+//    private String kakaoDevUserOAuthId;
+//
+//    @Value("${custom.security.oauth2.client.registration.naver.devUserOauthId}")
+//    private String naverDevUserOAuthId;
+//
+//    @Value("${custom.security.oauth2.client.registration.google.devUserOauthId}")
+//    private String googleDevUserOAuthId;
+//
+//    @Value("${custom.security.oauth2.client.registration.facebook.devUserOauthId}")
+//    private String facebookDevUserOAuthId;
+//
+//    @Bean
+//    CommandLineRunner initData(
+//            MemberService memberService,
+//            InstaMemberService instaMemberService,
+//            LikeablePersonService likeablePersonService
+//    ) {
+//        return new CommandLineRunner() {
+//            @Override
+//            @Transactional
+//            public void run(String... args) throws Exception {
+//                Member memberAdmin = memberService.join("admin", "1234").getData();
+//                Member memberUser1 = memberService.join("user1", "1234").getData();
+//                Member memberUser2 = memberService.join("user2", "1234").getData();
+//                Member memberUser3 = memberService.join("user3", "1234").getData();
+//                Member memberUser4 = memberService.join("user4", "1234").getData();
+//                Member memberUser6 = memberService.join("user6", "1234").getData();
+//                Member memberUser7 = memberService.join("user7", "1234").getData();
+//                Member memberUser8 = memberService.join("user8", "1234").getData();
+//                Member memberUser9 = memberService.join("user9", "1234").getData();
+//                Member memberUser10 = memberService.join("user10", "1234").getData();
+//
+//
+//                Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2745252925".formatted(kakaoDevUserOAuthId)).getData();
+//                Member memberUser7ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__%s".formatted(googleDevUserOAuthId)).getData();
+//                Member memberUser8ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__%s".formatted(naverDevUserOAuthId)).getData();
+//                Member memberUser9ByFacebook = memberService.whenSocialLogin("FACEBOOK", "FACEBOOK__%s".formatted(facebookDevUserOAuthId)).getData();
+//
+//                instaMemberService.connect(memberUser2, "insta_user2", "M");
+//                instaMemberService.connect(memberUser3, "insta_user3", "W");
+//                instaMemberService.connect(memberUser4, "e_hana_na", "M");
+//                instaMemberService.connect(memberUser6, "insta_user6", "W");
+//                instaMemberService.connect(memberUser7, "insta_user7", "W");
+//                instaMemberService.connect(memberUser8, "insta_user8", "W");
+//                instaMemberService.connect(memberUser9, "insta_user9", "M");
+//                instaMemberService.connect(memberUser10, "insta_user10", "W");
+//
+//                // 원활한 테스트와 개발을 위해서 자동으로 만들어지는 호감이 삭제, 수정이 가능하도록 쿨타임해제
+//                LikeablePerson likeablePersonToInstaUser4 = likeablePersonService.like(memberUser3, "e_hana_na", 1).getData();
+//                Ut.reflection.setFieldValue(likeablePersonToInstaUser4, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
+//                LikeablePerson likeablePersonToInstaUser100 = likeablePersonService.like(memberUser3, "insta_user100", 2).getData();
+//                Ut.reflection.setFieldValue(likeablePersonToInstaUser100, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
+//
+//                LikeablePerson likeablePersonToInstaUserAbcd = likeablePersonService.like(memberUser3, "insta_user_abcd", 2).getData();
+//                LikeablePerson likeablePersonToInstaUser6 = likeablePersonService.like(memberUser6, "e_hana_na", 1).getData();
+//                LikeablePerson likeablePersonToInstaUser7 = likeablePersonService.like(memberUser7, "e_hana_na", 3).getData();
+//                LikeablePerson likeablePersonToInstaUser8 = likeablePersonService.like(memberUser8, "e_hana_na", 1).getData();
+//                LikeablePerson likeablePersonToInstaUser9 = likeablePersonService.like(memberUser9, "e_hana_na", 3).getData();
+//                LikeablePerson likeablePersonToInstaUser10 = likeablePersonService.like(memberUser10, "e_hana_na", 2).getData();
+//
+//            }
+//        };
+//    }
+//}
